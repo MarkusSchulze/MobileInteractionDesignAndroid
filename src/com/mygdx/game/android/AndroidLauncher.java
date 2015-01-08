18da2f134c283030fce1976bbac5ffc04de47c07
MarkusSchulze <Markus-Schulze@gmx.net>
2015-01-08 11:19:32 +0100

    start

---
+++ src/com/mygdx/game/android/AndroidLauncher.java
@@ -0,0 +1,20 @@
+package com.mygdx.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.game.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		// config.u useGL20 = true;
+		config.useAccelerometer = true;
+		config.useCompass = true;
+
+		initialize(new MyGdxGame(), config);
+	}
+}
